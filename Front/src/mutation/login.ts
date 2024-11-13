e1aac26d9afc27c62ab95a17b226f07696a22791
faks <87184157+fran-galic@users.noreply.github.com>
2024-11-14 00:24:37 +0100

    ispravlejne i dodane jos neke stvari 2

--- Front/src/mutation/login.ts
+++ Front/src/mutation/login.ts
@@ -5,14 +5,12 @@ interface IregisterSucess {
   success: string;
 }
 
-export async function logIn(
-  url: string,
-  { arg }: { arg: ILogIn }
-) {
+export async function logIn(url: string, { arg }: { arg: ILogIn }) {
   const data = await fetcher<IregisterSucess>(url, {
     method: 'POST',
     body: JSON.stringify(arg),
   });
   /*  localStorage.setItem('user-id', (data as IUser).user.id); */
+  console.log('podaci sa servera za login: ', data);
   return data;
 }
