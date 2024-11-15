c3b8be8102114c365fc9a721609233f3d3ab34dd
Jakov <jb54616@fer.hr>
2024-11-16 00:41:00 +0100

    fixed swrKeys for deploy

--- Front/src/fetchers/swrKeys.ts
+++ Front/src/fetchers/swrKeys.ts
@@ -1,5 +1,5 @@
-const auth = 'http://127.0.0.1:8000/api/auth/';
-const home = 'http://127.0.0.1:8000/api/home/';
+const auth = 'https://easyrent-t7he.onrender.com/api/auth/';
+const home = 'https://easyrent-t7he.onrender.com/api/home/';
 
 export const swrKeys = {
   registerUser: `${auth}registerUser/`,
