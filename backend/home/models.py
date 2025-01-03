c505a483d656642b057b2e038039c16286cbf44a
XpORPID <ivan.dujmic.sb@gmail.com>
2025-01-04 00:40:15 +0100

    New models to string and unique on review.rent

--- backend/home/models.py
+++ backend/home/models.py
@@ -70,13 +70,17 @@ class Rent(models.Model):
     dateTimeRented = models.DateTimeField()
     dateTimeReturned = models.DateTimeField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return "Rentoid: " + self.rentoid + " Vehicle: " + self.vehicle + " Rented: " + self.dateTimeRented + " Returned: " + self.dateTimeReturned
 
 class Review(models.Model):
     review_id = models.AutoField(primary_key=True)
-    rent = models.ForeignKey(Rent, on_delete=models.CASCADE)
+    rent = models.ForeignKey(Rent, on_delete=models.CASCADE, unique=True)
     rating = models.SmallIntegerField(blank=True, default=None, null=True)
     description = models.TextField(blank=True, default='')
     reviewDate = models.DateField(blank=True, default=None, null=True)
+    def __str__(self):
+        return "RentID: " + self.rent + " Rating: " + self.rating + " Date: " + self.reviewDate
 """
 Zapisi u recenziji se dodaju u trenutku kada se definira dateTimeReturned 
 i ostaju prazni dok ih rentoid ne ispuni što može biti u bilo kojem trenutku u budućnosti.
