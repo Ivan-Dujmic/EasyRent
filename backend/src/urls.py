aa8c57fdb1dc81a36da2b02adc202c3471131567
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-01-15 19:44:23 +0100

    this is cheating

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -6,10 +6,17 @@ from . import views
 
 app_name = "src"
 urlpatterns = [
-    path("api/auth/registerUser/", csrf_exempt(views.registerUser), name="registerUser"),
-    path("api/auth/registerCompany/", csrf_exempt(views.registerCompany), name="registerCompany"),
+    path(
+        "api/auth/registerUser/", csrf_exempt(views.registerUser), name="registerUser"
+    ),
+    path(
+        "api/auth/registerCompany/",
+        csrf_exempt(views.registerCompany),
+        name="registerCompany",
+    ),
     path("api/auth/logoutUser/", views.logoutUser, name="logoutUser"),
     path("api/auth/loginUser/", csrf_exempt(views.loginUser), name="loginUser"),
-    path('activate/<uidb64>/<token>/', views.activate, name="activateUser"),
-    path("", views.redirectHome, name="redirectHome")
+    path("activate/<uidb64>/<token>/", views.activate, name="activateUser"),
+    path("", views.redirectHome, name="redirectHome"),
+    path("SuccessfulLogin#/", views.redictHome, name="redirectHome"),
 ]
