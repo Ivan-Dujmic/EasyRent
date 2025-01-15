47c2b37ac7e4d760ff54a79ff7da1811b0b1e770
faks <87184157+fran-galic@users.noreply.github.com>
2025-01-15 12:05:19 +0100

    dodao da se i dohvaceni automobili dodaju u localStorage (za CarContext)

--- Front/src/context/CarContext.tsx
+++ Front/src/context/CarContext.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { ICar } from '@/fetchers/homeData';
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from 'react';
 
 interface CarContextType {
   cars: ICar[] | null;
@@ -13,7 +19,21 @@ const CarContext = createContext<CarContextType | undefined>(undefined);
 
 // Provider za wrapanje aplikacije
 export const CarProvider = ({ children }: { children: ReactNode }) => {
-  const [cars, setCars] = useState<ICar[] | null>(null);
+  const [cars, setCarsState] = useState<ICar[] | null>(() => {
+    // Prilikom inicijalizacije pokušaj dohvatiti automobile iz localStorage
+    const savedCars = localStorage.getItem('cars');
+    return savedCars ? JSON.parse(savedCars) : null;
+  });
+
+  // Funkcija koja ažurira stanje i localStorage
+  const setCars = (newCars: ICar[]) => {
+    setCarsState(newCars);
+    localStorage.setItem('cars', JSON.stringify(newCars)); // Spremi automobile u localStorage
+  };
+
+  useEffect(() => {
+    // Ovdje možeš postaviti dodatnu logiku ako je potrebno pri učitavanju komponenti
+  }, []);
 
   return (
     <CarContext.Provider value={{ cars, setCars }}>
