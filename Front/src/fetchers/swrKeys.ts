195fb0f0296665bb7acbc9191a9bb83c8fbebc85
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-01-15 20:38:51 +0100

    Testing 1

--- Front/src/fetchers/swrKeys.ts
+++ Front/src/fetchers/swrKeys.ts
@@ -2,10 +2,10 @@ const auth = 'https://easyrent-t7he.onrender.com/api/auth/';
 const home = 'https://easyrent-t7he.onrender.com/api/home/';
 
 export const swrKeys = {
-  registerUser: `${auth}registerUser/`,
-  registerCompany: `${auth}registerCompany/`,
-  logIn: `${auth}loginUser/`,
-  showcased: `${home}showcased/`,
-  search: `${home}search`,
-  userinfo: `${auth}user-info/`,
+    registerUser: `${auth}register-user/`,
+    registerCompany: `${auth}register-company/`,
+    logIn: `${auth}login/`,
+    showcased: `${home}showcased/`,
+    search: `${home}search`,
+    userinfo: `${auth}user-info/`,
 };
