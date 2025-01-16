12e55539e994ce22178b4718a238ccae6a5031e6
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-01-16 04:50:43 +0100

    qfix

--- Front/src/fetchers/swrKeys.ts
+++ Front/src/fetchers/swrKeys.ts
@@ -1,7 +1,7 @@
-// const auth = 'https://easyrent-t7he.onrender.com/api/auth/';
-// const home = 'https://easyrent-t7he.onrender.com/api/home/';
-const auth = "http://127.0.0.1:8000/api/auth/";
-const home = "http://127.0.0.1:8000/api/home/"
+const auth = 'https://easyrent-t7he.onrender.com/api/auth/';
+const home = 'https://easyrent-t7he.onrender.com/api/home/';
+// const auth = "http://127.0.0.1:8000/api/auth/";
+// const home = "http://127.0.0.1:8000/api/home/"
 export const swrKeys = {
     registerUser: `${auth}register-user/`,
     registerCompany: `${auth}register-company/`,
