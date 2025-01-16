1189f13fcd84eb8740fab5e8c1fedc83006b5507
faks <87184157+fran-galic@users.noreply.github.com>
2025-01-16 21:57:56 +0100

    rjeseni merage konflikti 2

--- Front/src/fetchers/swrKeys.ts
+++ Front/src/fetchers/swrKeys.ts
@@ -3,20 +3,10 @@ const home = 'https://easyrent-t7he.onrender.com/api/home/';
 // const auth = "http://127.0.0.1:8000/api/auth/";
 // const home = "http://127.0.0.1:8000/api/home/"
 export const swrKeys = {
-<<<<<<< HEAD
-    registerUser: `${auth}register-user/`,
-    registerCompany: `${auth}register-company/`,
-    logIn: `${auth}login/`,
-    showcased: `${home}showcased/`,
-    search: `${home}search`,
-    userinfo: `${auth}user-info/`,
-    logout: `${auth}logout/`,
-=======
   registerUser: `${auth}registerUser/`,
   registerCompany: `${auth}registerCompany/`,
   logIn: `${auth}loginUser/`,
   showcased: `${home}showcased/`,
   search: (queryString: string = ''): string => `${home}search?${queryString}`, // search sa tipovima
   userinfo: `${auth}user-info/`,
->>>>>>> front-fg
 };
