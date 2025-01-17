f56f13f7fdd5e1771c6502ef3bc906af75ae47bf
Dzanija Ivan <ivan.dzanija222@gmail.com>
2025-01-17 01:07:38 +0100

    Adding photos

--- Front/src/fetchers/swrKeys.ts
+++ Front/src/fetchers/swrKeys.ts
@@ -3,9 +3,9 @@ const home = 'https://easyrent-t7he.onrender.com/api/home/';
 // const auth = "http://127.0.0.1:8000/api/auth/";
 // const home = "http://127.0.0.1:8000/api/home/"
 export const swrKeys = {
-    registerUser: `${auth}registerUser/`,
-    registerCompany: `${auth}registerCompany/`,
-    logIn: `${auth}loginUser/`,
+    registerUser: `${auth}register-user/`,
+    registerCompany: `${auth}register-company/`,
+    logIn: `${auth}login/`,
     showcased: `${home}showcased/`,
     search: (queryString: string = ''): string => `${home}search?${queryString}`, // search sa tipovima
     userinfo: `${auth}user-info/`,
